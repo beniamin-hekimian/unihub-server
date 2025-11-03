77b7e605b4035a6163ccefe28d7ff65808854a9a
beniamin-hekimian <penig300404@gmail.com>
2025-11-03 12:20:21 +0300

    config: connectDB setup

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
+const connectDB = require("./config/database");
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
+
+// Middleware to parse JSON
+app.use(express.json());
 
 app.get("/", (_, res) => {
-  res.send("Hello from Express!");
+  res.send("Hello from the server!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+async function startServer() {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`✅ Server is running on port: ${PORT}`);
+  });
+}
+
+startServer();
